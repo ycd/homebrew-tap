1f970f91464c172a5509c441b1e03fb1a8bafe24
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-11-21 04:41:47 +0300

    bump: release v0.3.1
    Signed-off-by: Yagiz Degirmenci <yagizcanilbey1903@gmail.com>

--- Formula/dstp.rb
+++ Formula/dstp.rb
@@ -2,27 +2,27 @@ require 'rbconfig'
 class Dstp < Formula
   desc "Run common networking tests against your site"
   homepage "https://github.com/ycd/dstp"
-  version "0.3.0"
+  version "0.3.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.0/dstp_0.3.0_darwin_x86_64.tar.gz"
-      sha256 "9f39fa6ef582280fbebcfe8d043895f8c916d1784c7ba8df6fd6c2b428a6b6eb"
+      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_darwin_x86_64.tar.gz"
+      sha256 "8e4c9af28f8446e50893506f16e44b4c0c38f96802dd4771103ad872cac151aa"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.0/dstp_0.3.0_darwin_arm64.tar.gz"
-      sha256 "452b695dda9f06e11429aa6408f1abf3a9a74e1ee11aa49bc3bb280650e7ec12"
+      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_darwin_arm64.tar.gz"
+      sha256 "6ca674a896ab129db2bdb82f9095d2856a9d2b5933f4e6a6cc83ef141c55292e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.0/dstp_0.3.0_linux_x86_64.tar.gz"
-      sha256 "ed8bf4fc5267d4fa72fdb066a8c6862b7d60c98b2452a5408bb43edeabe5cfe0"
+      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_linux_x86_64.tar.gz"
+      sha256 "f6bcb3705cfb91b20e715255de086227367533052b7f2f0913ca3a5b3f8770ef"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.0/dstp_0.3.0_linux_arm64.tar.gz"
-      sha256 "7b7374172ffe218cb465104e687c187c3943c7c6fa5f533e9d3f11a102bdbf47"
+      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_linux_arm64.tar.gz"
+      sha256 "3ae41c27025520c53e9e99ed2d6aca8852906987754ec4b0193822cf6ab077d5"
     end
   end
 
