0d1b9b1027f1e52a33857e6fd122d6418323d799
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-11-25 03:07:32 +0300

    bump: v0.4.0
    Signed-off-by: Yagiz Degirmenci <yagizcanilbey1903@gmail.com>

--- Formula/dstp.rb
+++ Formula/dstp.rb
@@ -2,27 +2,27 @@ require 'rbconfig'
 class Dstp < Formula
   desc "Run common networking tests against your site"
   homepage "https://github.com/ycd/dstp"
-  version "0.3.1"
+  version "0.4.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_darwin_x86_64.tar.gz"
-      sha256 "8e4c9af28f8446e50893506f16e44b4c0c38f96802dd4771103ad872cac151aa"
+      url "https://github.com/ycd/dstp/releases/download/v0.4.0/dstp_0.4.0_darwin_x86_64.tar.gz"
+      sha256 "dc4101eeae468d36982185833b8dd7af0a56ae7f33ee553b96c999773fbc5030"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_darwin_arm64.tar.gz"
-      sha256 "6ca674a896ab129db2bdb82f9095d2856a9d2b5933f4e6a6cc83ef141c55292e"
+      url "https://github.com/ycd/dstp/releases/download/v0.4.0/dstp_0.4.0_darwin_arm64.tar.gz"
+      sha256 "2472bfe1dbf0a0dc3a642a14a6c77e121317b4cc7126927bc1fcc2293542c9f9"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_linux_x86_64.tar.gz"
-      sha256 "f6bcb3705cfb91b20e715255de086227367533052b7f2f0913ca3a5b3f8770ef"
+      url "https://github.com/ycd/dstp/releases/download/v0.4.0/dstp_0.4.0_linux_x86_64.tar.gz"
+      sha256 "492caee303e62f5e012e84aa1d925570dfd37780054a68baaadae7779791303a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ycd/dstp/releases/download/v0.3.1/dstp_0.3.1_linux_arm64.tar.gz"
-      sha256 "3ae41c27025520c53e9e99ed2d6aca8852906987754ec4b0193822cf6ab077d5"
+      url "https://github.com/ycd/dstp/releases/download/v0.4.0/dstp_0.4.0_linux_arm64.tar.gz"
+      sha256 "ade3aeae239ffdcd617a68458e616cd3b64bfaadd846853f91e8b51858555b8e"
     end
   end
 
