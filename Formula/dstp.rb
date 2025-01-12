e5e54ff1a09e7943f8ac5f7f35dd6ec539a07d2c
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2025-01-12 12:02:46 +0300

    feature: update checksums for latest release
    Signed-off-by: Yagiz Degirmenci <yagizcanilbey1903@gmail.com>

--- Formula/dstp.rb
+++ Formula/dstp.rb
@@ -7,22 +7,22 @@ class Dstp < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/ycd/dstp/releases/download/v0.4.23/dstp_0.4.23_Darwin_amd64.tar.gz"
-      sha256 "dc4101eeae468d36982185833b8dd7af0a56ae7f33ee553b96c999773fbc5030"
+      sha256 "7bf240ccc7fcacc0905f231c7ee0b519d997ff0f42111610c1ff5af529d31520"
     end
     if Hardware::CPU.arm?
       url "https://github.com/ycd/dstp/releases/download/v0.4.23/dstp_0.4.23_Darwin_arm64.tar.gz" 
-      sha256 "2472bfe1dbf0a0dc3a642a14a6c77e121317b4cc7126927bc1fcc2293542c9f9"
+      sha256 "960eed0b4c2614d6ec493cebd5cd81a3119d9c21def6c90e1cef97d9f0a89db4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/ycd/dstp/releases/download/v0.4.23/dstp_0.4.23_Linux_amd64.tar.gz"
-      sha256 "492caee303e62f5e012e84aa1d925570dfd37780054a68baaadae7779791303a"
+      sha256 "247e68a0fd9558d8751b155d13d1f18668ae22d549fb1e156e9cccc7325833ef"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/ycd/dstp/releases/download/v0.4.23/dstp_0.4.23_Linux_arm64.tar.gz"
-      sha256 "ade3aeae239ffdcd617a68458e616cd3b64bfaadd846853f91e8b51858555b8e"
+      sha256 "cf379b4d0fcdf8b987be1a481d1f4b86a90cfc54232fd675d04aa35a9c2df6dc"
     end
   end
 
